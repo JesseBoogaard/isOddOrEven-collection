89c0fdc670507b820f5be746900965140a13300b
I403755 <j.boogaard@student.fontys.nl>
2018-10-22 18:56:18 +0200

    added improved JS version

---
+++ JS/1.js
@@ -0,0 +1,56 @@
+function isOddOrEven(x) {
+    let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    let arr = x.toString().split("")
+    let res;
+    y = arr.slice(-1)[0]
+    
+    for(let i = 0; i < numbers.length; i++){
+        if(y === numbers[i]){
+           res = numbers[i] 
+        }
+    }
+    switch(res){
+        case '0':
+            console.log(`${ arr.join("") } is even`)
+            break;
+
+        case '2':
+            console.log(`${ arr.join("") } is even`)
+            break;
+
+        case '4':
+            console.log(`${ arr.join("") } is even`)
+            break;
+
+        case '6':
+            console.log(`${ arr.join("") } is even`)
+            break;
+
+        case '8':
+            console.log(`${ arr.join("") } is even`)
+            break;
+
+        case '1':
+            console.log(`${ arr.join("") } is odd`)
+            break;
+        
+        case '3':
+            console.log(`${ arr.join("") } is odd`)
+            break;
+        
+        case '5':
+            console.log(`${ arr.join("") } is odd`)
+            break;
+
+        case '7':
+            console.log(`${ arr.join("") } is odd`)
+            break;
+
+        case '9':
+            console.log(`${ arr.join("") } is odd`)
+            break;
+    }
+}
+
+isOddOrEven(123)
+isOddOrEven(2048)
