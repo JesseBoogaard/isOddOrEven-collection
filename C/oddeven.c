47dd5c499018a038a6c13cd860df1510055f1959
ktejas <krishnasalimeda@gmail.com>
2018-10-25 17:54:08 +0530

    Added 2 types of Odd-Even checks in C with indentation fix

--- C/oddeven.c
+++ C/oddeven.c
@@ -5,25 +5,23 @@
 
 int isOddOrEven( const int i_n )
 {
-    if ( i_n & 1 )
-		printf( "%d is Odd\n", i_n );
-    else
-		printf( "%d is Even\n", i_n );
+    if ( i_n & 1 ) printf( "%d is Odd\n", i_n );
+    else printf( "%d is Even\n", i_n );
 }
 
 int isOdd( const long long i_n )
 {
     int n = i_n;
     for( int i = 6; i; i-- )
-	{
-        n *= n;
+    {
+	n *= n;
     }
     return !!n;
 }
 
 void printOddEven( const int i_n )
 {
-	printf( isOdd( i_n ) ? "%d is Odd\n" :  "%d is Even\n", i_n );
+    printf( isOdd( i_n ) ? "%d is Odd\n" :  "%d is Even\n", i_n );
 }
 
 
@@ -31,9 +29,8 @@ int main()
 {
     isOddOrEven( 13 );
     isOddOrEven( 48 );
-	
-	printOddEven( 13 );
-	printOddEven( 48 );
+    printOddEven( 13 );
+    printOddEven( 48 );
 
     return 0;
 }
