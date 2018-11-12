d81bebdd2aa975af50fac2e366e6428e790c7821
William Miles <wmpmiles@gmail.com>
2018-11-13 05:54:55 +1100

    Collatz BFS complete.

--- Python/hailstoneBFS.py
+++ Python/hailstoneBFS.py
@@ -4,46 +4,40 @@ def oddable(x):
     if x < 4:
         return False
     else:
-        val = int((x - 1) / 3)
-        if (3 * val + 1 == x):
-            # shhh
-            return val % 2 == 1
-        else:
-            return False
+        return 3 * int((x-1) / 3) + 1 == x
 
 def odd(x):
     if x == 1:
         return True
 
-    visited = {}
 
     h = []
+    heappush(h, (1, 1))
 
-    heappush(h, 1)
+    visited = {}
 
     while True:
-        val = heappop(h)
+        val, isOdd = heappop(h)
 
         visited[val] = None
 
         even = val * 2;
-
         if x == even:
             return False
         elif even not in visited:
-            heappush(h, even)
+            heappush(h, (even, 0))
 
-        if oddable(val):
+        if isOdd == 0 and oddable(val):
             o = int((val - 1) / 3)
 
             if x == o:
                 return True
             elif o not in visited:
-                heappush(h, o)
+                heappush(h, (o, 1))
 
 
 if __name__ == '__main__':
-    for x in range(1, 100):
+    for x in range(1, 1000):
         if odd(x) != x % 2 == 1:
             print(x)
 
